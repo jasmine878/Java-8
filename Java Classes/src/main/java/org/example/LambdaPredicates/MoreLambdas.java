fe6c88e39e9e014b5be334622924646b792313aa
Tae Hui Chang <jasmine878@yahoo.com>
2025-03-29 00:59:19 -0500

    Implement a Predicate functionoal interface

---
+++ Java Classes/src/main/java/org/example/LambdaPredicates/MoreLambdas.java
@@ -0,0 +1,24 @@
+package org.example.LambdaPredicates;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoreLambdas {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>(List.of("one", "two", "three", "four", "five"));
+
+//        Predicate<String> filter = (String string) -> string.startsWith("t");
+//        Predicate<String> filter = string -> string.startsWith("t");
+//        Predicate<String> filter = string -> !string.startsWith("t");
+//        strings.removeIf(filter);
+
+        strings.removeIf(string -> !string.startsWith("t"));
+
+
+//        Consumer<String> action = (String string) -> System.out.println(string);
+//        Consumer<String> action = string -> System.out.println(string);
+//        strings.forEach(action);
+
+        strings.forEach(action -> System.out.println(action));
+    }
+}
