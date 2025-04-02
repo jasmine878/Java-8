554a520b35c143adb4025ed80d4a9bf178694221
Tae Hui Chang <jasmine878@yahoo.com>
2025-04-01 22:50:23 -0500

    Compare method with autoboxing

---
+++ Java Classes/src/main/java/org/example/LambdaPerformance/AutoBoxingPerformance.java
@@ -0,0 +1,12 @@
+package org.example.LambdaPerformance;
+
+import java.util.Comparator;
+
+public class AutoBoxingPerformance {
+
+    //Comparator uses Integers
+    Comparator<Integer> cmp = (i1, i2) -> Integer.compare(i1, i2);
+
+    //compare method uses ints
+    int compare = cmp.compare(10, 20);
+}
