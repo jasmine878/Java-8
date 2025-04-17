67ef7c1905542d7a04c229a35e6a4eeda56bb965
Tae Hui Chang <jasmine878@yahoo.com>
2025-04-16 23:19:38 -0500

    Sort array using a Function

--- Java Classes/src/main/java/org/example/LambdaExpression/Comparator/CreatingComparators.java
+++ Java Classes/src/main/java/org/example/LambdaExpression/Comparator/CreatingComparators.java
@@ -3,6 +3,7 @@ package org.example.LambdaExpression.Comparator;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 
 public class CreatingComparators {
     public static void main(String[] args) {
@@ -16,6 +17,13 @@ public class CreatingComparators {
         Comparator<String> cmpLength = (s1, s2) -> Integer.compare(s1.length(), s2.length());
 
         stringsArr.sort(cmpLength);
+        System.out.println(stringsArr);
+
+        Function<String, Integer> listByLength = number -> number.length();
+
+        Comparator<String> cmpLength2 = Comparator.comparing(listByLength);
+
+        stringsArr.sort(cmpLength2);
         System.out.println(stringsArr);
     }
 }
