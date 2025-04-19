05a8699c8af70ae67ea64ec6f82cfb78feff51d6
Tae Hui Chang <jasmine878@yahoo.com>
2025-04-19 01:11:30 -0500

    Use Comparator default method to sort by name and age

--- Java Classes/src/main/java/org/example/LambdaExpression/Comparator/UserComparator.java
+++ Java Classes/src/main/java/org/example/LambdaExpression/Comparator/UserComparator.java
@@ -11,15 +11,24 @@ public class UserComparator {
         User sarah = new User("Sarah", 28);
         User james = new User("James", 35);
         User mary = new User("Mary", 33);
-        User john = new User("John", 24);
+        User john1 = new User("John", 25);
+        User john2 = new User("John", 24);
 
-        List<User> users = Arrays.asList(sarah, james, mary, john);
+        List<User> users = Arrays.asList(sarah, james, mary, john1, john2);
 
         Comparator<User> cmp = Comparator.comparing(user -> user.getName());
         users.sort(cmp);
 
         System.out.println(users);
         System.out.println();
+        users.forEach(user -> System.out.println(user));
+        System.out.println();
+
+        Comparator<User> cmpName = Comparator.comparing(user -> user.getName());
+        Comparator<User> cmpAge = Comparator.comparing(user -> user.getAge());
+        Comparator<User> cmpNameAge = cmpName.thenComparing(cmpAge);
+        users.sort(cmpNameAge);
+
         users.forEach(user -> System.out.println(user));
     }
 }
