41d4915d0bc939ef9ce8900205d36211de4d4a6b
Tae Hui Chang <jasmine878@yahoo.com>
2025-04-20 13:38:20 -0500

    Filter an array list using a Predicate

--- Java Classes/src/main/java/org/example/LambdaExpression/Predicates/MoreLambdas.java
+++ Java Classes/src/main/java/org/example/LambdaExpression/Predicates/MoreLambdas.java
@@ -1,10 +1,30 @@
 package org.example.LambdaExpression.Predicates;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class MoreLambdas {
     public static void main(String[] args) {
+
+        String[] colors = {"red", "green", "blue", "orange", "purple", "yellow"};
+        ArrayList<String> aList1 = new ArrayList<>(Arrays.asList(colors));
+        ArrayList<String> aList2 = new ArrayList<>(Arrays.asList(colors));
+        ArrayList<String> aList3 = new ArrayList<>(Arrays.asList(colors));
+
+        Predicate<String> predicate = new Predicate<String>() {
+            @Override
+            public boolean test(String color) {
+                return color.endsWith("e");
+            }
+        };
+
+        aList1.removeIf(predicate);
+        aList1.forEach(color -> System.out.println(" - " + color));
+        System.out.println();
+
+
         List<String> strings = new ArrayList<>(List.of("one", "two", "three", "four", "five"));
 
 //        Predicate<String> filter = (String string) -> string.startsWith("t");
@@ -12,6 +32,7 @@ public class MoreLambdas {
 //        Predicate<String> filter = string -> !string.startsWith("t");
 //        strings.removeIf(filter);
 
+        // Using Predicate with a Lambda expression
         strings.removeIf(string -> !string.startsWith("t"));
 
 
@@ -19,6 +40,7 @@ public class MoreLambdas {
 //        Consumer<String> action = string -> System.out.println(string);
 //        strings.forEach(action);
 
+        // Using Predicate with a Lambda expression
         strings.forEach(action -> System.out.println(action));
     }
 }
