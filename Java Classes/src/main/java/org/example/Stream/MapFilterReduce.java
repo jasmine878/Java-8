b9eada6584de29f31b215d88e9a5f19d29c9ec67
Tae Hui Chang <jasmine878@yahoo.com>
2025-05-30 15:33:26 -0500

    Create a stream of Users

---
+++ Java Classes/src/main/java/org/example/Stream/MapFilterReduce.java
@@ -0,0 +1,25 @@
+package org.example.Stream;
+
+import org.example.User;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class MapFilterReduce {
+    public static void main(String[] args) {
+        User p1 = new User("Paul", 25);
+        User p2 = new User("Sarah", 27);
+        User p3 = new User("James", 31);
+        User p4 = new User("Julie", 25);
+        User p5 = new User("Charles", 22);
+        User p6 = new User("Charlotte", 31);
+        User p7 = new User("Ann", 27);
+        User p8 = new User("Boris", 29);
+        User p9 = new User("Emily", 34);
+
+        List<User> people = List.of(p1, p2, p3, p4, p5, p6, p7, p8, p9);
+
+        Stream<User> stream =  people.stream();
+
+    }
+}
