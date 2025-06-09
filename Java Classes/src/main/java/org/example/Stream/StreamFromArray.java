389fde2b016063d2a2a98576ae2c500dd0666dd6
Tae Hui Chang <jasmine878@yahoo.com>
2025-06-08 22:25:35 -0500

    Create a Stream from an Array

--- Java Classes/src/main/java/org/example/Stream/StreamFromArray.java
+++ Java Classes/src/main/java/org/example/Stream/StreamFromArray.java
@@ -2,6 +2,7 @@ package org.example.Stream;
 
 import org.example.User;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class StreamFromArray {
@@ -14,7 +15,11 @@ public class StreamFromArray {
 
         User[] people = {p1, p2, p3, p4, p5};
 
+        //Create a Stream using Stream.of()
         long count = Stream.of(people).count();
         System.out.println("Count = " + count);
+
+        //Create a Stream using Arrays.stream()
+        Arrays.stream(people).forEach(p -> System.out.println(p));
     }
 }
