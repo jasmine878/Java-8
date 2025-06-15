8a446ea2479f13ee667bc419464accf23151d55c
Tae Hui Chang <jasmine878@yahoo.com>
2025-06-14 21:36:11 -0500

    Skip and Limit a Stream

--- Java Classes/src/main/java/org/example/Stream/SkippingAndLimiting.java
+++ Java Classes/src/main/java/org/example/Stream/SkippingAndLimiting.java
@@ -5,6 +5,8 @@ import java.util.stream.IntStream;
 public class SkippingAndLimiting {
     public static void main(String[] args) {
         IntStream.range(0, 30)
+                .skip(10)
+                .limit(11)
                 .forEach(index -> System.out.print(index + " "));
     }
 }
