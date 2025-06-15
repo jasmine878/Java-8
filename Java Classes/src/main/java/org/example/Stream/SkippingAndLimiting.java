e5842edacc86e49ac72cb55f44ee5ff790298e45
Tae Hui Chang <jasmine878@yahoo.com>
2025-06-14 22:26:34 -0500

    Skip and Limit a Stream

--- Java Classes/src/main/java/org/example/Stream/SkippingAndLimiting.java
+++ Java Classes/src/main/java/org/example/Stream/SkippingAndLimiting.java
@@ -1,6 +1,10 @@
 package org.example.Stream;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class SkippingAndLimiting {
     public static void main(String[] args) {
@@ -8,5 +12,14 @@ public class SkippingAndLimiting {
                 .skip(10)
                 .limit(11)
                 .forEach(index -> System.out.print(index + " "));
+        System.out.println();
+
+        Path path = Path.of("Java Classes/src/main/java/org/example/firstNames.txt");
+
+        try(Stream<String> lines = Files.lines(path)) {
+            lines.skip(5).limit(10).forEach(name -> System.out.print(name + " "));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
